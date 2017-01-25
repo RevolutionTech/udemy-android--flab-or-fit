599534c17c8ee61e3d89a4633111c2eac2186595
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-24 21:00:42 -0800

    Create the details activity

---
+++ app/src/main/java/com/revolutiontech/flaborfit/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.revolutiontech.flaborfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exerciseImage = (ImageView)findViewById(R.id.exerciseImage);
+    }
+}
