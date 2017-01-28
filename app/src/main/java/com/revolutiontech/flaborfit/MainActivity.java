58bb427cc1b28b914147e6c9462a39801f10a8df
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-28 15:32:32 -0800

    Create overflow menu in MainActivity

--- app/src/main/java/com/revolutiontech/flaborfit/MainActivity.java
+++ app/src/main/java/com/revolutiontech/flaborfit/MainActivity.java
@@ -3,6 +3,8 @@ package com.revolutiontech.flaborfit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.RelativeLayout;
 
@@ -42,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
                 loadDetailActivity(MainActivity.EXERCISE_CARDIO);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.items, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void loadDetailActivity(String exerciseTitle) {
