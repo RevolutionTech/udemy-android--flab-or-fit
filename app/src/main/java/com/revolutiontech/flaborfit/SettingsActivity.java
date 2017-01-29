c1d2a3a39d59ea493cdd0c11de655d95f1a4c62a
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-28 17:01:20 -0800

    Add logic to change MainActivity background color
    depending on the user's night mode setting

--- app/src/main/java/com/revolutiontech/flaborfit/SettingsActivity.java
+++ app/src/main/java/com/revolutiontech/flaborfit/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
 
+    public static final String KEY_PREF_NIGHT_MODE = "pref_night_mode";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
